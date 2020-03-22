0ddf67b27cf9e2be1822afeeef22e73d4466393a
AquaSecure <100585091+AquaSecure@users.noreply.github.com>
2020-03-22 16:07:32 +0000

    Make it build.

--- OneForAll/data/build.py
+++ OneForAll/data/build.py
@@ -63,4 +63,68 @@ class MultiTaskDataLoader(object):
     def __next__(self):
         batch = {}
 
-        if sel
+        if self.cfg.sample_mode == 'batch':
+            for name, iter_ in self.task_iters.items():
+                batch[name] = next(iter_)
+        elif self.cfg.sample_mode == 'sample':
+            name = random.choices(self.task_iters.keys(), self.cfg.sample_prob)[0]
+            batch[name] = next(self.task_iters[name])
+        else:
+            raise NotImplementedError
+
+        return batch
+
+    # Signal for shutting down background thread
+    def shutdown(self):
+        """shutdown
+        """
+        for name, loader in self.task_loaders:
+            loader.shutdown()
+
+
+class MOEplusplusMultiTaskDataLoader(MultiTaskDataLoader):
+    """MOEplusplusMultiTaskDataLoader
+    """
+    def __init__(self, task_loaders, cfg):
+        globalrank2taskid = cfg['globalrank2taskid']
+        rank = comm.get_rank()
+        taskid = globalrank2taskid[str(rank)]
+        taskname = list(task_loaders.keys())[taskid]
+        # task_loader = {taskname: task_loaders[taskname]}
+        print('rank {} has task_loader of {}'.format(rank, taskname))
+        # super().__init__(task_loader, cfg)
+
+        self.task_loaders = task_loaders
+        self.cfg = cfg
+
+        self.task_iters = {taskname: iter(task_loaders[taskname])}
+        # for name, loader in self.task_loaders.items():
+        #     self.task_iters[name] = iter(loader)
+
+
+def build_reid_test_loader_lazy(test_set, test_batch_size, num_workers, dp_degree=None, alive_rank_list=None):
+    """
+    build reid test_loader for tasks of Person, Veri and Sop
+    this test_loader only supports single gpu
+    """
+    if dp_degree is not None:
+        if (alive_rank_list is not None) and (comm.get_rank() not in alive_rank_list):
+            return {}
+        dp_group = None
+        moe_group = None
+    else:
+        dp_group = None
+        moe_group = None
+    mini_batch_size = test_batch_size // comm.get_world_size(dp_group)
+    data_sampler = samplers.InferenceSampler(test_set, dp_group=dp_group, moe_group=moe_group)
+    batch_sampler = paddle.io.BatchSampler(sampler=data_sampler, batch_size=mini_batch_size)
+    test_loader = paddle.io.DataLoader(
+        dataset=test_set,
+        batch_sampler=batch_sampler,
+        collate_fn=fast_batch_collator,
+        num_workers=0,
+        )
+    return test_loader
+
+
+def build_test_set(
