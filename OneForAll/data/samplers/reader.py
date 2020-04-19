07bfa9ca6f9a23a3344cb6a3bdb7c15461a42a3a
AquaSecure <100585091+AquaSecure@users.noreply.github.com>
2020-04-19 17:51:22 +0000

    experimenting

--- OneForAll/data/samplers/reader.py
+++ OneForAll/data/samplers/reader.py
@@ -38,4 +38,61 @@ class Compose(object):
                 if k not in ['RandomErasing', 'TimmAutoAugment']:
                     self.transforms_det.append(f)
 
-    def __c
+    def __call__(self, data):
+        if 'is_cls' in data and data['is_cls']:
+            for f in self.transforms_cls:
+                # skip TimmAutoAugment for vehicle color data
+                if type(f).__name__ == "TimmAutoAugment" and data['cls_label_1'] == -1:
+                    continue
+                try:
+                    data = f(data)
+                except Exception as e:
+                    stack_info = traceback.format_exc()
+                    logger.warning("fail to map sample transform [{}] "
+                                "with error: {} and stack:\n{}".format(
+                                    f, e, str(stack_info)))
+                    raise e
+        else:
+            for f in self.transforms_det:
+                try:
+                    data = f(data)
+                except Exception as e:
+                    stack_info = traceback.format_exc()
+                    logger.warning("fail to map sample transform [{}] "
+                                "with error: {} and stack:\n{}".format(
+                                    f, e, str(stack_info)))
+                    raise e
+        return data
+
+
+class BatchCompose(Compose):
+    def __init__(self, transforms, num_classes=80, collate_batch=True):
+        super(BatchCompose, self).__init__(transforms, num_classes)
+        self.collate_batch = collate_batch
+
+    def __call__(self, data):
+        for f in self.transforms_cls:
+            try:
+                data = f(data)
+            except Exception as e:
+                stack_info = traceback.format_exc()
+                logger.warning("fail to map batch transform [{}] "
+                               "with error: {} and stack:\n{}".format(
+                                   f, e, str(stack_info)))
+                raise e
+
+        # remove keys which is not needed by model
+        extra_key = ['h', 'w', 'flipped']
+        for k in extra_key:
+            for sample in data:
+                if k in sample:
+                    sample.pop(k)
+
+        # batch data, if user-define batch function needed
+        # use user-defined here
+        if self.collate_batch:
+            batch_data = default_collate_fn(data)
+        else:
+            batch_data = {}
+            for k in data[0].keys():
+    
