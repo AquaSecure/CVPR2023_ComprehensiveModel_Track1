2f44b3f79702c63d1b9e1ff5f4d2429db179ebf2
AquaSecure <100585091+AquaSecure@users.noreply.github.com>
2020-05-09 17:12:04 +0000

    v2.3.8

--- OneForAll/data/transforms/build.py
+++ OneForAll/data/transforms/build.py
@@ -142,4 +142,61 @@ def build_transforms_lazy(is_train=True, **kwargs):
         size_train = kwargs.get('size_train', [256, 128])
         # crop
         do_crop = kwargs.get('do_crop', False)
-        crop_size = kwargs.get('crop_size', [224, 
+        crop_size = kwargs.get('crop_size', [224, 224])
+        crop_scale = kwargs.get('crop_scale', [0.16, 1])
+        crop_ratio = kwargs.get('crop_ratio', [3. / 4., 4. / 3.])
+
+        # augmix augmentation
+        do_augmix = kwargs.get('do_augmix', False)
+        augmix_prob = kwargs.get('augmix_prob', 0.0)
+
+        # auto augmentation
+        do_autoaug = kwargs.get('do_autoaug', False)
+        autoaug_prob = kwargs.get('autoaug_prob', 0.0)
+
+        # horizontal filp
+        do_flip = kwargs.get('do_flip', False)
+        flip_prob = kwargs.get('flip_prob', 0.5)
+
+        # padding
+        do_pad = kwargs.get('do_pad', False)
+        padding_size = kwargs.get('padding_size', 10)
+        padding_mode = kwargs.get('padding_mode', 'constant')
+
+        # color jitter
+        do_cj = kwargs.get('do_cj', False)
+        cj_prob = kwargs.get('cj_prob', 0.5)
+        cj_brightness = kwargs.get('cj_brightness', 0.15)
+        cj_contrast = kwargs.get('cj_contrast', 0.15)
+        cj_saturation = kwargs.get('cj_saturation', 0.1)
+        cj_hue = kwargs.get('cj_hue', 0.1)
+
+        # random affine
+        do_affine = kwargs.get('do_affine', False)
+
+        # random erasing
+        do_rea = kwargs.get('do_rea', False)
+        rea_prob = kwargs.get('rea_prob', 0.5)
+        rea_value = list(kwargs.get('rea_value', [0.485 * 255, 0.456 * 255, 0.406 * 255]))
+
+        # random patch
+        do_rpt = kwargs.get('do_rpt', False)
+        rpt_prob = kwargs.get('rpt_prob', 0.5)
+
+        # normalize
+        mean = kwargs.get('mean', [0.485 * 255, 0.456 * 255, 0.406 * 255])
+        std = kwargs.get('std', [0.229 * 255, 0.224 * 255, 0.225 * 255])
+
+        if do_autoaug:
+            res.append(RandomApply(prob=autoaug_prob, transform_function_class=AutoAugment))
+
+        if size_train[0] > 0:
+            res.append(T.Resize(size_train[0] if len(size_train) == 1 else size_train, interpolation='bicubic'))
+
+        if do_crop:
+            res.append(T.RandomResizedCrop(size=crop_size[0] if len(crop_size) == 1 else crop_size,
+                                           interpolation='bicubic',
+                                           scale=crop_scale, ratio=crop_ratio))
+        if do_pad:
+            res.extend([T.Pad(padding_size, padding_mode=padding_mode),
+                        T.RandomCrop(size_train[0] if len(size_train) == 1 else size_tra
