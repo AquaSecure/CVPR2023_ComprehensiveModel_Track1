82f4db4f98bb9f16d66fee9e33db145112969a66
AquaSecure <100585091+AquaSecure@users.noreply.github.com>
2020-11-22 18:53:02 +0000

    Update to latest version.

---
+++ OneForAll/detectron2/evaluation/coco_evaluation.py
@@ -0,0 +1,24 @@
+# !/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+import contextlib
+import copy
+import io
+import itertools
+import json
+import logging
+import numpy as np
+import os
+import pickle
+from collections import OrderedDict
+import pycocotools.mask as mask_util
+import torch
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+from tabulate import tabulate
+
+import detectron2.utils.comm as comm
+from detectron2.config import CfgNode
+from detectron2.data import MetadataCatalog
+from detectron2.data.datasets.coco import convert_to_coco_json
+from detectron2.evaluation.fast_eval_api import COCOeval_opt
+from detectron2.structures impor
