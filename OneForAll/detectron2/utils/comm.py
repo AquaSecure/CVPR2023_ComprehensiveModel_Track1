7bb299f17d17b66ec8d62f8b0850ccef6a0399c7
AquaSecure <100585091+AquaSecure@users.noreply.github.com>
2021-01-24 18:33:26 +0000

    fix a comment

--- OneForAll/detectron2/utils/comm.py
+++ OneForAll/detectron2/utils/comm.py
@@ -38,4 +38,83 @@ def get_rank() -> int:
 def get_local_rank() -> int:
     """
     Returns:
-        The rank of the current process within the l
+        The rank of the current process within the local (per-machine) process group.
+    """
+    if not dist.is_available():
+        return 0
+    if not dist.is_initialized():
+        return 0
+
+    # return int(os.environ['LOCAL_RANK']) #dist.get_rank()
+    # assert (
+    #     _LOCAL_PROCESS_GROUP is not None
+    # ), "Local process group is not created! Please use launch() to spawn processes!"
+    # return dist.get_rank(group=_LOCAL_PROCESS_GROUP)
+    if _LOCAL_PROCESS_GROUP is not None:
+        return dist.get_rank(group=_LOCAL_PROCESS_GROUP)
+    else:
+        return int(os.environ['LOCAL_RANK']) #dist.get_rank()
+
+
+def get_local_size() -> int:
+    """
+    Returns:
+        The size of the per-machine process group,
+        i.e. the number of processes per machine.
+    """
+    if not dist.is_available():
+        return 1
+    if not dist.is_initialized():
+        return 1
+    return dist.get_world_size(group=_LOCAL_PROCESS_GROUP)
+
+
+def is_main_process() -> bool:
+    return get_rank() == 0
+
+
+def synchronize():
+    """
+    Helper function to synchronize (barrier) among all processes when
+    using distributed training
+    """
+    if not dist.is_available():
+        return
+    if not dist.is_initialized():
+        return
+    world_size = dist.get_world_size()
+    if world_size == 1:
+        return
+    if dist.get_backend() == dist.Backend.NCCL:
+        # This argument is needed to avoid warnings.
+        # It's valid only for NCCL backend.
+        # dist.barrier(device_ids=[torch.cuda.current_device()])
+        dist.barrier()
+    else:
+        dist.barrier()
+
+
+@functools.lru_cache()
+def _get_global_gloo_group():
+    """
+    Return a process group based on gloo backend, containing all the ranks
+    The result is cached.
+    """
+    if dist.get_backend() == "nccl":
+        return dist.new_group(backend="gloo")
+    else:
+        return dist.group.WORLD
+
+
+def _serialize_to_tensor(data, group):
+    backend = dist.get_backend(group)
+    assert backend in ["gloo", "nccl"]
+    device = torch.device("cpu" if backend == "gloo" else "cuda")
+
+    buffer = pickle.dumps(data)
+    if len(buffer) > 1024 ** 3:
+        logger = logging.getLogger(__name__)
+        logger.warning(
+            "Rank {} trying to all-gather {:.2f} GB of data on device {}".format(
+                get_rank(), len(buffer) / (1024 ** 3), device
+        
