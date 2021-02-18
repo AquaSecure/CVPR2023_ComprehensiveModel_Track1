1c8a97d276412ba88ae81d58a3ebf4aac4a9027c
AquaSecure <100585091+AquaSecure@users.noreply.github.com>
2021-02-18 20:49:42 +0000

    nuget package

--- OneForAll/evaluation/evaluator.py
+++ OneForAll/evaluation/evaluator.py
@@ -14,4 +14,64 @@ class DatasetEvaluator:
     Base class for a dataset evaluator.
     The function :func:`inference_on_dataset` runs the model over
     all samples in the dataset, and have a DatasetEvaluator to process the inputs/outputs.
-    This class will accumulate information of the inputs/outputs (by 
+    This class will accumulate information of the inputs/outputs (by :meth:`process`),
+    and produce evaluation results in the end (by :meth:`evaluate`).
+    """
+
+    def reset(self):
+        """
+        Preparation for a new round of evaluation.
+        Should be called before starting a round of evaluation.
+        """
+        pass
+
+    def preprocess_inputs(self, inputs):
+        """preprocess_inputs
+        """
+        
+        pass
+
+    def process(self, inputs, outputs):
+        """
+        Process an input/output pair.
+        Args:
+            inputs: the inputs that's used to call the model.
+            outputs: the return value of `model(input)`
+        """
+        pass
+
+    def evaluate(self):
+        """
+        Evaluate/summarize the performance, after processing all input/output pairs.
+        Returns:
+            dict:
+                A new evaluator class can return a dict of arbitrary format
+                as long as the user can process the results.
+                In our train_net.py, we expect the following format:
+                * key: the name of the task (e.g., bbox)
+                * value: a dict of {metric name: score}, e.g.: {"AP50": 80}
+        """
+        pass
+
+def inference_on_dataset(model, data_loader, evaluator, flip_test=False, moe_group=None):
+    """
+    Run model on the data_loader and evaluate the metrics with evaluator.
+    The model will be used in eval mode.
+    Args:
+        model (nn.Module): a module which accepts an object from
+            `data_loader` and returns some outputs. It will be temporarily set to `eval` mode.
+            If you wish to evaluate a model in `training` mode instead, you can
+            wrap the given model and override its behavior of `.eval()` and `.train()`.
+        data_loader: an iterable object with a length.
+            The elements it generates will be the inputs to the model.
+        evaluator (DatasetEvaluator): the evaluator to run. Use
+            :class:`DatasetEvaluators([])` if you only want to benchmark, but
+            don't want to do any evaluation.
+        flip_test (bool): If get features with flipped images
+    Returns:
+        The return value of `evaluator.evaluate()`
+    """
+    num_devices = comm.get_world_size()
+    logger = logging.getLogger(__name__)
+    if hasattr(data_loader, 'dataset'):
+        logger.info("Start inference on {} images".form
