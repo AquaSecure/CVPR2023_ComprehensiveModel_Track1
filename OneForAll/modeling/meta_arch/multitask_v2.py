bd39aa81cb779d6a9d55270ced89a48ba3f9e57b
AquaSecure <100585091+AquaSecure@users.noreply.github.com>
2023-10-04 05:39:39 +0000

    Add basic logging

---
+++ OneForAll/modeling/meta_arch/multitask_v2.py
@@ -0,0 +1,31 @@
+# !/usr/bin/env python3
+# Copyright (c) 2022  PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import random
+
+import paddle
+from paddle import nn
+
+from modeling.losses import triplet_loss, cross_entropy_loss, log_accuracy
+
+class MultiTaskBatchFuse(nn.Layer):
+    """
+    Baseline architecture. Any models that contains the following two components:
+    1. Per-image feature extraction (aka backbone)
+    2. Per-image feature aggregation and loss computation
+    """
+
+    def __init__(
+   
